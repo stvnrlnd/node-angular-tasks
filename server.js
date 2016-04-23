1ee658aa82c90256f1ca248790ed40b895e0ac51
Steven Roland <steven@mojave.io>
2016-04-23 17:39:29 -0400

    Tasks API
    - Created the API routes for the tasks.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var morgan         = require('morgan');
 
 // ------ Configure the application
 var app            = express();
+var api            = express.Router();
 var port           = process.env.PORT || 3000;
 var config         = require('./resource/config');
 var Task             = require('./resource/models/task');
@@ -23,6 +24,50 @@ mongoose.connect(config.dbURL);
 app.use(express.static(__dirname + '/base'));
 
 // ------ Build routes
+api.route('/tasks')
+  .get(function(req, res) {
+    Task.find(function(err, tasks) {
+      if (err){
+        res.send(err);
+      }
+      res.json(tasks);
+    });
+  })
+  .post(function(req, res) {
+    Task.create({
+      text : req.body.text,
+      done : false
+    }, function(err, task) {
+      if (err){
+        res.send(err);
+      }
+      Task.find(function(err, tasks) {
+        if (err){
+          res.send(err);
+        }
+        res.json(tasks);
+      });
+    });
+  });
+
+api.route('/tasks/:task_id')
+  .delete(function(req, res) {
+    Task.remove({
+      _id : req.params.task_id
+    }, function(err, task) {
+      if (err){
+        res.send(err);
+      }
+      Task.find(function(err, tasks) {
+        if (err){
+          res.send(err);
+        }
+        res.json(tasks);
+      });
+    });
+  });
+
+app.use('/api', api);
 
 // ------ Serve
 app.listen(port, function() {
