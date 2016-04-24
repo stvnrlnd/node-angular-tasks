22d3b049bd4dc172633b40235cca66dd41f48691
Steven Roland <steven@mojave.io>
2016-04-24 18:29:04 -0400

    Getting data with a service
    - Created an angular service to grab mock data for the application.

--- base/app/core.js
+++ base/app/core.js
@@ -1,11 +1,16 @@
 (function(){
   'use strict';
   angular.module('taskApp', [])
-    .controller('taskController', function($scope) {
-      $scope.tasks = [
-        {"text": "Code"},
-        {"text": "Code more"},
-        {"text": "Drink Coffee"},
-      ]
+    .controller('taskController', function($scope, taskService) {
+      taskService.getTasks(function(response) {
+        console.log(response.data);
+        $scope.tasks = response.data;
+      });
+    })
+    .service('taskService', function($http) {
+      this.getTasks = function(callback) {
+        $http.get('app/mock/tasks.json')
+          .then(callback);
+      };
     });
 }());
